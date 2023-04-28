c81e088a88a3c364d43255beb8d7acb91aec8746
Yohana Alexandra Becerra Becerra <jobysalex@gmail.com>
2023-04-27 19:55:05 -0500

    Primer Commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,53 @@
 import validator from './validator.js';
 
-console.log(validator);
+const tarjetas = [
+  { nombre: "American Express", numero: "3437" },
+  { nombre: "Master Card", numero: "6011" },
+  { nombre: "Diners Club", numero: "2131" },
+  { nombre: "JCB", numero: "1800" },
+];
+
+const numTarjeta = document.getElementById("numeroTarjeta");
+const botVerificar = document.getElementById("botonVerificar");
+const mensaje = document.getElementById("mensaje");
+const numeroEnmascarado = document.getElementById("numEnmascarado");
+
+numTarjeta.addEventListener('keypress', (e) => {
+  if (e.key === 'Enter') {
+    verificarNumTarjeta();
+  } else if (!escribirNum(e)) {
+    e.preventDefault();
+  }
+  const enmascarar = validator.maskify(numTarjeta.value);
+  numeroEnmascarado.textContent = enmascarar;
+});
+
+botVerificar.addEventListener('click', verificarNumTarjeta);
+function verificarNumTarjeta() {
+  if (numTarjeta.value.length < 16) {
+    mensaje.innerHTML = "Su Numero de Tarjeta esta incompleto";
+  }
+  else {
+    const tarjetaValida = validator.isValid(numTarjeta.value);
+    console.log(tarjetaValida);
+    const numero = numTarjeta.value.substr(0, 4);
+    const tarjeta = tarjetas.find((t) => t.numero === numero);
+    if (tarjetaValida && tarjeta) {
+      mensaje.innerHTML = `Su Tarjeta es: ${tarjeta.nombre}`;
+    } else {
+      mensaje.innerHTML = "Su Tarjeta NO es Valida";
+    }
+  }
+}
+
+function escribirNum(num) {
+  const key = num.charCode;
+  return key >= 48 && key <= 57;
+}
+
+const revesNumTarjeta = document.getElementById("revesNumeroTarjeta");
+
+numTarjeta.addEventListener("input", () => {
+  const numeroTarjeta = numTarjeta.value;
+  revesNumTarjeta.innerText = numeroTarjeta.split("").reverse().join("");
+});
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,43 @@
 const validator = {
-  // ...
-};
+  isValid: function (numeroTarjeta) {
+    console.log(numeroTarjeta);
+    const revesNumTarjeta = numeroTarjeta.split("").reverse();
+    let suma = 0;
+    for (let i = 0; i < revesNumTarjeta.length; i++) {
+      if (i % 2 !== 0) {
+        let multiplicacionPar = parseInt(revesNumTarjeta[i] * 2);
+        if (multiplicacionPar > 9) {
+          multiplicacionPar -= 9;
+        }
+        suma += multiplicacionPar;
+      } else {
+        suma += parseInt(revesNumTarjeta[i]);
+      }
+    }
+    if (suma % 10 === 0) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  },
 
+  maskify: function (numeroTarjeta) {
+    console.log(numeroTarjeta);
+    if (numeroTarjeta.length <= 4) {
+      return numeroTarjeta;
+    }
+    else if (numeroTarjeta.length > 4) {
+      const numeroGatos = numeroTarjeta.length - 4;
+      const numeroTarjetaVisible = numeroTarjeta.substring(numeroGatos);
+      let numeroTarjetaInvisible = "";
+      for (let i = 0; i < numeroGatos; i++) {
+        numeroTarjetaInvisible += "#";
+      }
+      const numeroTarjetaFinal = numeroTarjetaInvisible + numeroTarjetaVisible;
+      console.log(numeroTarjetaFinal);
+      return numeroTarjetaFinal;
+    }
+  }
+}
 export default validator;
